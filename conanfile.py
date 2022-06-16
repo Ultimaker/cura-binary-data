3b80d7d6fdf38a6bc30b124cc04160bc2b02450a
j.spijker@ultimaker.com <jelle spijker>
2022-06-16 20:08:52 +0200

    Place cura-binary-data under Conan version control
    Contributes to CURA-9365

---
+++ conanfile.py
@@ -0,0 +1,46 @@
+import os
+
+from conan import ConanFile
+from conans import tools
+from conan.errors import ConanInvalidConfiguration
+
+required_conan_version = ">=1.47.0"
+
+
+class CuraBinaryDataConan(ConanFile):
+    name = "cura_binary_data"
+    license = "LGPL-3.0"
+    author = "Ultimaker B.V."
+    url = "https://github.com/Ultimaker/cura-binary-data"
+    description = "Contains binary data for Cura releases, like compiled translations and firmware."
+    topics = ("conan", "binaries", "translation", "firmware", "cura")
+    build_policy = "missing"
+    exports = "LICENSE*"
+    settings = "os", "compiler", "build_type", "arch"
+    no_copy_source = True
+    scm = {
+        "type": "git",
+        "subfolder": ".",
+        "url": "auto",
+        "revision": "auto"
+    }
+
+    def layout(self):
+        self.folders.source = "."
+        self.folders.build = "."
+        self.folders.generators = os.path.join(self.folders.build, "conan")
+
+    def validate(self):
+        if tools.Version(self.version) <= tools.Version("4"):
+            raise ConanInvalidConfiguration("Only versions 5+ are support")
+
+    def package(self):
+        self.copy("*", src = "cura", dst = os.path.join(self.cpp.package.resdirs[0], "cura"))
+        self.copy("*", src = "uranium", dst = os.path.join(self.cpp.package.resdirs[0], "uranium"))
+
+        if self.settings.os == "Windows":
+            self.copy("*", src = "windows", dst = os.path.join(self.cpp.package.resdirs[0], "windows"))
+
+    def package_id(self):
+        del self.info.settings.compiler
+        del self.info.settings.build_type
